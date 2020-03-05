05e6e7b2e1651b1f39c50822eb6dcb25e744f863
Sebastian Matusiak <sebmat003@utp.edu.pl>
2020-03-05 17:06:14 +0100

    added "about" component and animation to several elements

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    let doc = document.documentElement.scrollTop;
+    if (doc > 400 && doc < 1600 ) {
+      document.getElementById('angular').classList.add('width-2');
+      document.getElementById('html').classList.add('width-3');
+      document.getElementById('css').classList.add('width-3');
+      document.getElementById('typescript').classList.add('width-3');
+      document.getElementById('photoshop').classList.add('width-4');
+      document.getElementById('illustrator').classList.add('width-3');
+    } else {
+      document.getElementById('angular').classList.remove('width-2');
+      document.getElementById('html').classList.remove('width-3');
+      document.getElementById('css').classList.remove('width-3');
+      document.getElementById('typescript').classList.remove('width-3');
+      document.getElementById('photoshop').classList.remove('width-4');
+      document.getElementById('illustrator').classList.remove('width-3');
+    }
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Sebastian Matusiak';
 
-  
+  ngOnInit() {
+    AOS.init();
+  }
 }
