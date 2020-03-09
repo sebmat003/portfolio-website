8d65f083de8479736c9588bb25f3cc875aec57e0
Sebastian Matusiak <sebmat003@utp.edu.pl>
2020-03-09 17:24:58 +0100

    added galleries with images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { WorkComponent } from './work/work.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import {MDBBootstrapModule} from 'angular-bootstrap-md';
-
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +18,7 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md';
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    NgxGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-work',
@@ -7,19 +8,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkComponent implements OnInit {
 
-  images = [
-    {
-      img: '../../../assests/images/mapy1.jpg', thumb: '../../../assests/images/mapy1.jpg', description: 'Mapy'
-    },
-    {
-      img: '../../../assests/images/mapy1.jpg', thumb: '../../../assests/images/mapy1.jpg', description: 'Mapy'
-    },
-  ];
+  galleryImages;
+  galleryDesign;
+  galleryOptions;
+  galleryOptionsTwo;
 
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.galleryOptions = [
+      {
+        width: '400px',
+        height: '400px',
+        thumbnailsColumns: 3,
+        imageAnimation: NgxGalleryAnimation.Fade,
+        preview: false,
+        thumbnailsArrows: true,
+        imageArrows: true
+      },
+      // max-width 800
+      {
+        breakpoint: 800,
+        width: '100%',
+        height: '600px',
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/images/mapy1.PNG',
+        medium: 'assets/images/mapy1.PNG',
+        big: 'assets/images/mapy1.PNG'
+      },
+      {
+        small: 'assets/images/mapy2.PNG',
+        medium: 'assets/images/mapy2.PNG',
+        big: 'assets/images/mapy2.PNG'
+      },
+      {
+        small: 'assets/images/mapy3.PNG',
+        medium: 'assets/images/mapy3.PNG',
+        big: 'assets/images/mapy3.PNG'
+      }
+    ];
+
+    this.galleryDesign = [
+      {
+        small: 'assets/images/sheldon.jpg',
+        medium: 'assets/images/sheldon.jpg',
+        big: 'assets/images/sheldon.jpg'
+      },
+      {
+        small: 'assets/images/kid-with-rose.jpg',
+        medium: 'assets/images/kid-with-rose.jpg',
+        big: 'assets/images/kid-with-rose.jpg'
+      },
+      {
+        small: 'assets/images/brosmoke.jpg',
+        medium: 'assets/images/brosmoke.jpg',
+        big: 'assets/images/brosmoke.jpg'
+      },
+      {
+        small: 'assets/images/brosmoke-cartoon.jpg',
+        medium: 'assets/images/brosmoke-cartoon.jpg',
+        big: 'assets/images/brosmoke-cartoon.jpg'
+      },
+      {
+        small: 'assets/images/logos.jpg',
+        medium: 'assets/images/logos.jpg',
+        big: 'assets/images/logos.jpg'
+      }
+    ];
+
+
   }
 
 }
